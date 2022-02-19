8612b3ec33b4245866d7c751d5dfb75e3c0e3ef7
diogoab <diogo.alves.barbosa@gmail.com>
2022-02-18 22:33:54 -0300

    fix app.py

--- app/app.py
+++ app/app.py
@@ -1,52 +1,65 @@
 # app.py
 
 import os
-
 import boto3
+from boto3.dynamodb.conditions import Key
 
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-client = boto3.client('dynamodb', region_name='us-east-1')
+AWS_REGION = os.getenv('AWS_REGION')
+ENDPOINT_URL = os.getenv('ENDPOINT_URL')
+
 dynamoTableName = 'bestMusic'
+
+if str(ENDPOINT_URL).upper() != 'AWS':
+    dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION, endpoint_url=ENDPOINT_URL)
+else:
+    dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION)
 
 @app.route("/")
 def hello():
-    return "Oi, Bem vindo a biblioteca dos anos 90!"
+    table = dynamodb.Table(dynamoTableName)
+    resp = table.scan()
+    HTML_ARTIST = ''
+    for item in resp.get('Items'):
+        HTML_ARTIST += '<a href="/v1/yourmusicof/90s/{0}">{0}</a><br>'.format(item.get('artist'))
+    return 'Oi, Bem vindo a biblioteca dos anos 90! <a href="/add">[ add ]</a><br><br>{0}'.format(HTML_ARTIST)
+
+@app.route("/add")
+def add_page():
+    return 'Oi, Bem vindo a biblioteca dos anos 90! <a href="/">[ back ]</a><br><br><form action="/v1/yourmusicof/90s" method="POST"> \
+  <label for="artist">Artist:</label> \
+  <input type="text" id="artist" name="artist"><br> \
+  <label for="song">Song:</label> \
+  <input type="text" id="song" name="song"><br> \
+  <input type="submit" value="Submit"> \
+</form>'
 
 
 @app.route("/v1/yourmusicof/90s/<string:artist>")
 def get_artist(artist):
-    resp = client.get_item(
-        TableName=dynamoTableName,
-        Key={
-            'artist': { 'S': artist }
-        }
-    )
-    item = resp.get('Item')
-    if not item:
+    table = dynamodb.Table(dynamoTableName)
+    resp = table.query(KeyConditionExpression=Key('artist').eq(artist))
+    items = resp.get('Items')
+    if not items:
         return jsonify({'error': 'Desculpe, mas esse artista não exist'}), 404
 
     return jsonify({
-        'artist': item.get('artist').get('S'),
-        'song': item.get('song').get('S')
+        'artist': items[0].get('artist'),
+        'song': items[0].get('song')
     })
 
 
 @app.route("/v1/yourmusicof/90s", methods=["POST"])
 def create_artist():
-    artist = request.json.get('artist')
-    song = request.json.get('song')
+    artist = request.form.get('artist')
+    song = request.form.get('song')
     if not artist or not song:
         return jsonify({'error': 'Oi, precisa informar sua música da banda preferida...'}), 400
 
-    resp = client.put_item(
-        TableName=dynamoTableName,
-        Item={
-            'artist': {'S': artist },
-            'song': {'S': song }
-        }
-    )
+    table = dynamodb.Table(dynamoTableName)
+    resp = table.put_item(Item={ 'artist': artist, 'song': song })
 
     return jsonify({
         'artist': artist,
