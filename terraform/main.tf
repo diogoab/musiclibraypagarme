62d6e5c3487cd8272a1c9768071613c71443d387
diogoab <diogo.alves.barbosa@gmail.com>
2022-02-18 23:44:06 -0300

    test destroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -455,7 +455,7 @@ resource "aws_ecs_service" "service" {
   iam_role        = aws_iam_role.ecs_service_role.arn
   depends_on      = [aws_lb_listener.alb_listener]
   load_balancer {
-    container_name   = "demo-app"
+    container_name   = "musiclibrarypagarme-app"
     container_port   = 5000
     target_group_arn = aws_lb_target_group.ecs_rest_api_tg.arn
   }
